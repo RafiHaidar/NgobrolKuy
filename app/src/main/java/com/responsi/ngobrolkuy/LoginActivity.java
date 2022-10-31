031c771e74260acfa2be7164339c057b14403436
Rafi Haidar <rafihaidararibawa@gmail.com>
2022-10-31 11:57:48 +0700

    Perubahan Responsi

--- app/src/main/java/com/responsi/ngobrolkuy/LoginActivity.java
+++ app/src/main/java/com/responsi/ngobrolkuy/LoginActivity.java
@@ -2,13 +2,42 @@ package com.responsi.ngobrolkuy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText etUser,etPass;
+    private Button buttonLogin, buttonNoAkun;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        etUser =findViewById(R.id.etusername);
+        etPass = findViewById(R.id.etpass);
+        buttonLogin = findViewById(R.id.btnLogin);
+        buttonNoAkun = findViewById(R.id.btnNoAkun);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindah_1 = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(pindah_1);
+            }
+        });
+
+        buttonNoAkun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindah_2 = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(pindah_2);
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/responsi/ngobrolkuy/RegisterActivity.java
+++ app/src/main/java/com/responsi/ngobrolkuy/RegisterActivity.java
@@ -2,13 +2,41 @@ package com.responsi.ngobrolkuy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText Username, Email, Password, ConfPass;
+    private Button Register, AdaAkun;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Username = findViewById(R.id.etusernamereg);
+        Email = findViewById(R.id.etemailreg);
+        Password = findViewById(R.id.etpassreg);
+        ConfPass = findViewById(R.id.etconfirmpassreg);
+        Register = findViewById(R.id.btnRegister);
+        AdaAkun = findViewById(R.id.btnAdaAkun);
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindah_3 = new Intent(RegisterActivity.this, ProfilePicActivity.class);
+                startActivity(pindah_3);
+            }
+        });
+        AdaAkun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindah_4 = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(pindah_4);
+            }
+        });
     }
 }
